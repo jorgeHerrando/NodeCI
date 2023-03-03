76ddef6f326edb69623bd82e2437b0bce733a065
Jorge Herrando <jorge.herrando@adidas.com>
2023-03-03 11:23:25 +0100

    travis config

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,8 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI:
+    'mongodb://jherrandodev:fZX6JzdRSfvi5GiU@ac-dmdkuxh-shard-00-00.1f8akoe.mongodb.net:27017,ac-dmdkuxh-shard-00-01.1f8akoe.mongodb.net:27017,ac-dmdkuxh-shard-00-02.1f8akoe.mongodb.net:27017/blog-dev?ssl=true&replicaSet=atlas-vzpqol-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379',
 };
